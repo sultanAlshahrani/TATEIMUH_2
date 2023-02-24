fbf9291ab857106625465763089f28ab4245ec46
sultan <sultan_33_886@outlook.sa>
2023-02-24 21:50:45 +0300

    Define pages and paths

--- Vaccinations/main/urls.py
+++ Vaccinations/main/urls.py
@@ -5,5 +5,12 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
-        path("", views.home_page, name="home_page"),
+     path("", views.home_page, name="home_page"),
+     path("profile/", views.profile_page, name="profile_page"),
+     path("vaccination_Schedule/", views.vaccination_schedule_page, name="vaccination_schedule_page"),
+     path("vaccination_reminders/", views.vaccination_reminders_page, name="vaccination_reminders_page"),
+     path("vaccination_service/", views.vaccination_service_page, name="vaccination_service_page"),
+     path("communication_service/", views.communication_service_page, name="communication_service_page"),
+
+
 ]
--- Vaccinations/main/views.py
+++ Vaccinations/main/views.py
@@ -5,7 +5,27 @@ from django.http import HttpRequest, HttpResponse
  
 def home_page(request : HttpRequest):
 
-    return render(request,'main/home_page.html')
+    return render(request,'main/home.html')
+
+def profile_page(request : HttpRequest):
+
+    return render(request,'main/profile.html')
+
+def vaccination_schedule_page(request : HttpRequest):
+
+    return render(request,'main/vaccination_Schedule.html')
+
+def vaccination_reminders_page(request : HttpRequest):
+
+    return render(request,'main/vaccination_reminders.html')
+
+def vaccination_service_page(request : HttpRequest):
+
+    return render(request,'main/vaccination_service.html')
+
+def communication_service_page(request : HttpRequest):
+
+    return render(request,'main/communication_service.html')
 
 
 
